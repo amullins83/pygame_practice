658f0a19f73f96eec2e69e2e9734688afdd94411
Austin Mullins <austin.mullins@gmail.com>
2013-07-18 14:27:25 -0500

    I think I fixed hit detection. Next task is showing the score.

--- hitDetection.py
+++ hitDetection.py
@@ -20,16 +20,28 @@ class HitDetector(object):
         return self.didCollideLeft(thing1, thing2) or self.didCollideRight(thing1, thing2)
 
     def didCollideTop(self, thing1, thing2):
-        return thing1.velocity.y > thing2.velocity.y and thing1.bottom() >= thing2.top() and self.inside(thing1, thing2)
+        return thing1.velocity.y > thing2.velocity.y and \
+        thing1.bottom() >= thing2.top() and \
+        (thing1.bottom() - thing2.top()) <= (thing1.velocity.y - thing2.velocity.y) and \
+        self.inside(thing1, thing2)
 
     def didCollideBottom(self, thing1, thing2):
-        return thing1.velocity.y < thing2.velocity.y and thing1.top() <= thing2.bottom() and self.inside(thing1, thing2)
+        return thing1.velocity.y < thing2.velocity.y and \
+        thing1.top() <= thing2.bottom() and \
+        (thing2.bottom() - thing1.top()) <= (thing2.velocity.y - thing1.velocity.y) and \
+        self.inside(thing1, thing2)
 
     def didCollideLeft(self, thing1, thing2):
-        return thing1.velocity.x > thing2.velocity.x and thing1.right() >= thing2.left() and self.inside(thing1, thing2)
+        return thing1.velocity.x > thing2.velocity.x and \
+        thing1.right() >= thing2.left() and \
+        (thing1.right() - thing2.left()) <= (thing1.velocity.x - thing2.velocity.x) and \
+        self.inside(thing1, thing2)
 
     def didCollideRight(self, thing1, thing2):
-        return thing1.velocity.x < thing2.velocity.x and thing1.left() <= thing2.right() and self.inside(thing1, thing2)
+        return thing1.velocity.x < thing2.velocity.x and \
+        thing1.left() <= thing2.right() and \
+        (thing2.right() - thing1.left()) <= (thing2.velocity.x - thing1.velocity.x) and \
+        self.inside(thing1, thing2)
 
     def inside(self, thing1, thing2):
         return self.insideHoriz(thing1, thing2) and self.insideVert(thing1, thing2)
